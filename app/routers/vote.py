f137783c74dcfccc2ee97572108bd9fccc5cbb7e
Arjun Singh <admin@arjunsingh.com.np>
2022-01-21 23:42:55 +0545

    fix vote bug

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -1,4 +1,4 @@
-from fastapi import status, HTTPException, Depends, APIRouter
+from fastapi import status, HTTPException, Depends, APIRouter, Response
 from sqlalchemy.orm import Session
 
 from .. import schemas, database, models, oauth2
@@ -9,6 +9,9 @@ router = APIRouter(prefix='/vote', tags=['Vote'])
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Post Not Found')
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
     vote_found = vote_query.first()
     if vote.dir == 1:
